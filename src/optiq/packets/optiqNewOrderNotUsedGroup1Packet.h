c300a5e16e6f79ee461f3c310f9e37b01a2f552c
Ryan Eastwood <ryan.eastwood@neueda.com>
2019-04-30 15:47:38 +0100

    new 109 packets

---
+++ src/optiq/packets/optiqNewOrderNotUsedGroup1Packet.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014-2018 Neueda Ltd.
+ * 
+ * Generated 14:16:38 30/04/19
+ */
+#ifndef _OPTIQ_NEWORDERNOTUSEDGROUP1_PACKET_H_
+#define _OPTIQ_NEWORDERNOTUSEDGROUP1_PACKET_H_
+
+#include <string>
+#include <sstream>
+#include <stdint.h>
+
+#include "optiqConstants.h"
+#include "optiqPacketUtils.h"
+/* #include "OptiqMessageHeaderPacket.h" */
+
+
+namespace neueda
+{
+
+PACKED(class optiqNewOrderNotUsedGroup1Packet
+{
+public:
+
+    optiqNewOrderNotUsedGroup1Packet ()
+    {
+    }
+
+
+});
+
+} // namespace neueda
+
+#endif
