96f5304a5b2bf119a1eb71e5687f4c1c3aa04f73
pwj58 <>
2021-10-08 17:56:48 +0100

    prevent core if setup fails to load swxcodec

--- test/swx/swxCodecTest.cpp
+++ test/swx/swxCodecTest.cpp
@@ -29,7 +29,10 @@ protected:
 
     virtual void TearDown ()
     {
-        delete mSwx;
+        if ( mSwx != NULL )
+        {
+            delete mSwx;
+        }
     }
 
     codecFactory mFactory;
